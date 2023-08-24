c4024e92b69ebcaead3e8c1f986604fa962e26d3
errikrn <errikrn@gmail.com>
2023-08-24 21:58:49 +0700

    edited body json and form-data

--- handler/scoreHandler.go
+++ handler/scoreHandler.go
@@ -12,124 +12,90 @@ import (
 func CreateScore(ctx *gin.Context) {
 	db := database.GetDB()
 	contentType := helpers.GetContentType(ctx)
-
-	Score := models.Scores{}
+	score := models.Scores{}
 
 	if contentType == appJSON {
-		ctx.ShouldBindJSON(&Score)
+		if err := ctx.ShouldBindJSON(&score); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input", "message": err.Error()})
+			return
+		}
 	} else {
-		ctx.ShouldBind(&Score)
+		if err := ctx.ShouldBind(&score); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input", "message": err.Error()})
+			return
+		}
 	}
 
-	err := db.Debug().Create(&Score).Error
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error":   "Bad request",
-			"message": err.Error(),
-		})
+	if err := db.Debug().Create(&score).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Database error", "message": err.Error()})
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"data": Score,
-	})
+	ctx.JSON(http.StatusOK, gin.H{"success": true, "data": score})
+}
+
+func GetAllScores(ctx *gin.Context) {
+	db := database.GetDB()
+
+	var scores []models.Scores
+	if err := db.Debug().Find(&scores).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error", "message": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"success": true, "data": scores})
 }
 
 func UpdateScore(ctx *gin.Context) {
 	db := database.GetDB()
 	contentType := helpers.GetContentType(ctx)
-
-	var updatedScore models.Scores
+	scoreID := ctx.Param("id")
+	updatedScore := models.Scores{}
 
 	if contentType == appJSON {
-		err := ctx.ShouldBindJSON(&updatedScore)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{
-				"error":   "Bad request",
-				"message": err.Error(),
-			})
+		if err := ctx.ShouldBindJSON(&updatedScore); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input", "message": err.Error()})
 			return
 		}
 	} else {
-		err := ctx.ShouldBind(&updatedScore)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{
-				"error":   "Bad request",
-				"message": err.Error(),
-			})
+		if err := ctx.ShouldBind(&updatedScore); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input", "message": err.Error()})
 			return
 		}
 	}
 
 	var existingScore models.Scores
-	scoreID := ctx.Param("id")
-
 	if err := db.Debug().Where("id = ?", scoreID).First(&existingScore).Error; err != nil {
-		ctx.JSON(http.StatusNotFound, gin.H{
-			"error": "Score not found",
-		})
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "Score not found"})
 		return
 	}
 
 	existingScore.AssignmentTitle = updatedScore.AssignmentTitle
 	existingScore.Description = updatedScore.Description
+	existingScore.Score = updatedScore.Score
 
 	if err := db.Debug().Save(&existingScore).Error; err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"error":   "Database error",
-			"message": err.Error(),
-		})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Database error", "message": err.Error()})
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"success": true,
-		"data":    existingScore,
-	})
-}
-
-func GetAllScoreByStudent(ctx *gin.Context) {
-	db := database.GetDB()
-
-	var scores []models.Scores
-	if err := db.Debug().Find(&scores).Error; err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"error":   "Internal Server Error",
-			"message": err.Error(),
-		})
-		return
-	}
-
-	ctx.JSON(http.StatusOK, gin.H{
-		"success": true,
-		"data":    scores,
-	})
+	ctx.JSON(http.StatusOK, gin.H{"success": true, "data": existingScore})
 }
 
 func DeleteScore(ctx *gin.Context) {
 	db := database.GetDB()
-
 	scoreID := ctx.Param("id")
-	var score models.Scores
+	score := models.Scores{}
 
 	if err := db.Debug().Where("id = ?", scoreID).First(&score).Error; err != nil {
-		ctx.JSON(http.StatusNotFound, gin.H{
-			"error":   "Score not found",
-			"message": err.Error(),
-		})
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "Score not found", "message": err.Error()})
 		return
 	}
 
 	if err := db.Debug().Delete(&score).Error; err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"error":   "Internal Server Error",
-			"message": err.Error(),
-		})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error", "message": err.Error()})
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"success": true,
-		"message": "Score deleted successfully",
-	})
+	ctx.JSON(http.StatusOK, gin.H{"success": true, "message": "Score deleted successfully"})
 }
